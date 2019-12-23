8deaee8abe20c8ddf6f6917e18b09cb6a6b25085
Rafael de Moura Moreira <rafaelmmoreira@gmail.com>
2019-12-23 15:11:03 -0300

    Update reversi.py

--- reversi.py
+++ reversi.py
@@ -2,11 +2,12 @@
 REVERSI
 by Rafael de Moura Moreira
 rafaelmmoreira@gmail.com
-Available at https://github.com/rafaelmmoreira
+Available at https://github.com/rafaelmmoreira/PyReversiTK
 
 A simple implementation of Reversi in Python using tkinter.
 Made just for fun to celebrate 10 years from my first
-programming assignment in college: Reversi in C.
+programming assignment in college: Reversi in C, which
+is also available: https://github.com/rafaelmmoreira/Reversi-C
 '''
 
 from tkinter import *
